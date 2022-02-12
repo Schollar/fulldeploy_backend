746fa5d815c21dc832a736ca36d80f97ad43a8ae
schollar <cschollar22@gmail.com>
2022-02-12 09:36:09 -0700

    Added get request endpoint to get all jokes from DB

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, Response
+import json
+import dbhandler as dbh
+import sys
+app = Flask(__name__)
+
+
+@app.get('/api/jokes')
+def get_posts():
+    try:
+        success, jokes = dbh.get_jokes()
+        jokes_json = json.dumps(jokes, default=str)
+    except:
+        return Response("Something went wrong getting the jokes from the DB!", mimetype="application/json", status=400)
+    if(success):
+        return Response(jokes_json, mimetype="application/json", status=200)
+    else:
+        return Response("Something went wrong getting the jokes from the DB!", mimetype="application/json", status=400)
+
+
+if(len(sys.argv) > 1):
+    mode = sys.argv[1]
+else:
+    print('You must pass a mode to run this script. Either testing or production')
+    exit()
+# Depending on what mode is passed, we check and run the appropriate code.
+if(mode == "testing"):
+    print('Running in testing mode!')
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
+elif(mode == "production"):
+    print('Running in production mode')
+    import bjoern  # type: ignore
+    bjoern.run(app, "0.0.0.0", 5005)
+else:
+    print('Please Run in either testing or production')
---
+++ dbhandler.py
@@ -0,0 +1,50 @@
+import dbcreds
+import mariadb as db
+
+
+def db_connect():
+    conn = None
+    cursor = None
+    try:
+        conn = db.connect(user=dbcreds.user, password=dbcreds.password,
+                          host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+    except db.OperationalError:
+        print('Something is wrong with the DB')
+    except:
+        print('Something went wrong connecting to the DB')
+    return conn, cursor
+# Disconnect function that takes in the conn and cursor and attempts to close both
+
+
+def db_disconnect(conn, cursor):
+    try:
+        cursor.close()
+    except:
+        print('Error closing cursor')
+    try:
+        conn.close()
+    except:
+        print('Error closing connection')
+
+# Get posts function gets the jokes from the DB and returns them.
+
+
+def get_jokes():
+    success = False
+    jokes = []
+    conn, cursor = db_connect()
+    try:
+        cursor.execute(
+            "SELECT id, content FROM joke")
+        jokes = cursor.fetchall()
+        success = True
+    except db.OperationalError:
+        print('Something is wrong with the db!')
+    except db.ProgrammingError:
+        print('Error running DB query')
+    except:
+        print('Error with the DB')
+    db_disconnect(conn, cursor)
+
+    return success, jokes
